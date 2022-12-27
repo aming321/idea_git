066d43cda60c77087f198f21411c0d229e4ef7fb
aming <1346905186@qq.com>
2022-12-27 17:30:43 +0800

    idea first commit

---
+++ src/main/java/com/aming/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.aming.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.aming.git
+ * Description:
+ *
+ * @author aming
+ * @create 2022/12/27-17:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
